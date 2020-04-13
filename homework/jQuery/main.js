1d4072b59e3f8803a1292024873d6146f8587241
naomylelis <nxl339@miami.edu>
2020-04-13 18:35:23 +0100

    new tingz

--- homework/jQuery/main.js
+++ homework/jQuery/main.js
@@ -10,30 +10,32 @@ $(document).ready(function() {
     $("#title").html("Guide to <em>Lisbon, Portugal</em>");
   });
 
-});
+  $("#navigation").mouseover(function() {
+    $("navigation").css("background-color", "#B0BF1A")
 
-$("#navigation").mouseover(function() {
-  $("navigation").css("background-color", "#B0BF1A")
+  });
 
-});
+  $("#button1").click(function() {
+
+    $("#mustvisit").toggle();
+
+  });
+
+  $("#button2").click(function() {
+    // stuff we put here will happen when you click button 2.
+
+    $("#title").removeClass("big-text");
+  });
+
+  $("#tips").mouseover(function() {
+    $("#tips").css("font-size", "50px");
+
+  });
+
+  $("#tips").mouseleave(function() {
+    $("#tips").css("font-size", "100%");
 
 
-$("#button1").click(function() {
-
-  $("#mustvisit").show("text");
-
-});
-
-$("#button2").click(function() {
-  // stuff we put here will happen when you click button 2.
-
-  $("#title").removeClass("big-text");
-});
-
-$("#tips").mouseover(function() {
-
-$("#tips").css("font-size", "50");
-
-});
+  });
 
 });
