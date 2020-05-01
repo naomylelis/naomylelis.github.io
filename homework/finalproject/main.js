dde72c0c5e6c9ff23dba5dee45f8c0656728aa82
naomylelis <nxl339@miami.edu>
2020-05-01 23:58:06 +0100

    updated
    updated HTML, CSS and jQuery

---
+++ homework/finalproject/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  console.log("we loaded");
+
+
+  $("#button1").click(function() {
+
+    $("#lost-id").toggle();
+
+  });
+
+  $("#button2").click(function() {
+
+    $("#alternative-id").toggle();
+  });
+
+  $("#button3").click(function() {
+
+    $("#bars-id").toggle();
+  });
+
+  $("#button4").click(function() {
+
+    $("#pop-id").toggle();
+  });
+
+
+
+});
